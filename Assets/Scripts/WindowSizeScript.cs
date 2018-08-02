ccc05c99c53265a86bddf0f954d7148e8b08682e
saltire sable <saltiresable@gmail.com>
2018-08-01 20:00:11 -0400

    New 2d game with 64x64 camera

---
+++ Assets/Scripts/WindowSizeScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowSizeScript : MonoBehaviour {
+	public int windowSize = 512;
+
+	void Start() {
+		Screen.SetResolution(windowSize, windowSize, false);
+	}
+
+	void Update() {
+
+	}
+}
