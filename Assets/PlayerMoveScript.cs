b105e7070b4395a999b0686abb772f1dd8b4fe26
saltire sable <saltiresable@gmail.com>
2018-08-21 23:25:21 -0400

    Make player square, add frame, set origin at screen centre

--- Assets/PlayerMoveScript.cs
+++ Assets/PlayerMoveScript.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum sides { BOTTOM, LEFT, TOP, RIGHT };
+
 public class PlayerMoveScript : MonoBehaviour {
 	public float walkSpeed = 1;
 	public float jumpSpeed = 7;
 	public float gravity = 1;
-	public float margin = 3;
+	public int frameThickness = 1;
+
+	int side = (int)sides.BOTTOM;
+	int groundY;
+	float halfWidth;
+	float height;
 
 	float hspeed = 0;
 	float vspeed = 0;
@@ -14,20 +21,28 @@ public class PlayerMoveScript : MonoBehaviour {
 	SpriteRenderer sprite;
 
 	void Start() {
+		groundY = -32 + frameThickness;
+
 		sprite = GetComponentInChildren<SpriteRenderer>();
+
+		BoxCollider2D collider = GetComponent<BoxCollider2D>();
+		halfWidth = collider.size.x / 2;
+		height = collider.size.y;
 	}
 
 	void FixedUpdate() {
 		hspeed = Input.GetAxis("Horizontal") * walkSpeed;
 
-		if (transform.position.y == 0 && Input.GetAxis("Jump") > 0) {
+		if (transform.position.y == groundY && Input.GetAxis("Jump") > 0) {
 			vspeed += jumpSpeed;
 		}
 		vspeed -= gravity;
 
-		hspeed = Mathf.Min(hspeed, 32 - margin - transform.position.x);
-		hspeed = Mathf.Max(hspeed, -32 + margin - transform.position.x);
-		vspeed = Mathf.Max(vspeed, -transform.position.y);
+		// Prevent player from moving past the walls or floor.
+		hspeed = Mathf.Max(hspeed, groundY + halfWidth - transform.position.x); // left
+		hspeed = Mathf.Min(hspeed, -groundY - halfWidth - transform.position.x); // right
+		vspeed = Mathf.Max(vspeed, groundY - transform.position.y); // bottom
+		vspeed = Mathf.Min(vspeed, -groundY - height - transform.position.y); // top
 
 		transform.position += new Vector3(hspeed, vspeed, 0);
 
