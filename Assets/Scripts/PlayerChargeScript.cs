3becbd15e9ef972e2b2af0a3a2bd8f99a8f1c5b7
saltire sable <saltiresable@gmail.com>
2018-08-23 01:55:39 -0400

    Player can charge, with a glow that pulses when fully charged

---
+++ Assets/Scripts/PlayerChargeScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerChargeScript : MonoBehaviour {
+	public float chargeTime = 1;
+	public Color glowColor;
+
+	public float pulseFrequency = 8;
+	public float pulseAmount = .1f;
+
+	float chargeTimeElapsed = 0;
+
+	Material mat;
+
+	void Start() {
+		mat = GetComponentInChildren<SpriteRenderer>().material;
+	}
+
+	void Update() {
+		if (Input.GetButton("Charge")) {
+			chargeTimeElapsed += Time.deltaTime;
+
+			float chargeAmount = Mathf.Clamp01(chargeTimeElapsed / chargeTime);
+			float alpha = chargeAmount * (1 - pulseAmount);
+
+			if (chargeTimeElapsed >= chargeTime) {
+				alpha += Mathf.Sin(Mathf.PI * (chargeTimeElapsed - chargeTime) * pulseFrequency) * pulseAmount;
+			}
+
+			mat.SetColor("_EmissionColor", Color.Lerp(Color.clear, glowColor, alpha));
+		}
+		else if (Input.GetButtonUp("Charge")) {
+			chargeTimeElapsed = 0;
+			mat.SetColor("_EmissionColor", Color.clear);
+		}
+	}
+}
--- Assets/Scripts/PlayerMoveScript.cs
+++ Assets/Scripts/PlayerMoveScript.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerMoveScript : MonoBehaviour {
 	public float walkSpeed = 1;
 	public float jumpSpeed = 7;
